98a71d20b5c6f6e743d6704dd194c6dda554267c
Nikitoss1205 <nikitosska2004@gmail.com>
2020-06-17 00:38:18 +0300

    step_2 (slider)

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            }]
+    });
+  });
