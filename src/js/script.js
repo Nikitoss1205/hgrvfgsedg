0e40e6703db08d722f92785f5088fd006c04ec23
Nikitoss1205 <nikitosska2004@gmail.com>
2020-06-17 23:12:15 +0300

    step_4

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
+	
+	// slider
+
+	$('.carousel__inner').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.png"></button>',
         responsive: [{
@@ -11,11 +14,15 @@ $(document).ready(function(){
         }]
 	});
 
+	// tabs
+
 	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
 		$(this)
 		  .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
 		  .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 	});
+
+	// catalog
 
 	function toggleSlide(item) {
 		$(item).each(function(i) {
@@ -29,6 +36,23 @@ $(document).ready(function(){
 
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
+
+	// modal
+
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn()
+	});
+
+	$('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #thanks, #order-1, #order-2, #order-3').fadeOut()
+	});
+
+	$('.button_catalog').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn();
+		});
+	});
 });
 
   
